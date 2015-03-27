3e21bda5c56da616d9d3e823096f297dfd7c9981
el coruco <el.coruco@gmail.com>
2015-03-26 22:35:11 -0600

    inicia fideicomisos

---
+++ application/models/fideicomisos_model.php
@@ -0,0 +1,22 @@
+<?php
+// FIDEICOMISOS 2015
+// date     : 26/03/2015
+// @package : fideicomisos
+// @file    : fideicomisos_model.php
+// @version : 1.0.0
+// @author  : Gobierno fácil <howdy@gobiernofacil.com>
+// @url     : http://gobiernofacil.com
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+/*
+* 
+* T H E   M O D E L 
+* --------------------------------------------------------------------------------
+* Aquí está el SQL de todos los queries a la DB de fideicomisos
+*
+*/
+class fideicomisos_model extends CI_Model{
+  public function __construct(){
+    parent::__construct();
+  }
+}
