5e572cd9daaac2b291388167703feaaf0af108fc
hugovom <hugo.om@gmail.com>
2015-06-22 20:35:11 -0500

    update nav

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="fide-menu fide-menu-vertical fide-menu-right" id="fide-menu-s2">
 	<h3>Menú</h3>
-	<a href="index.html" class="current">Inicio</a>
+	<a href="<?= url();?>" class="current">Inicio</a>
 	<a href="reportajes.html">Reportajes</a>
 	<a href="#">¿Qué es un fideicomiso?</a>
 	<a href="#">VizData</a>
@@ -14,7 +14,7 @@
 				<a href="http://www.eluniversal.com.mx/periodismo-datos/" class="eluniversal">El Universal | DATA</a>  
 			</div>
 			<div class="col-sm-6">
-				<h1>Quién Fondeó</h1> 
+				<h1><a href="<?= url();?>">Quién Fondeó</a></h1> 
 			</div>
 			<div class="col-sm-1 col-sm-offset-2">
 				<button id="showRightPush" class="menu-icon">☰</button>
