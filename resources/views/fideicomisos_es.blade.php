03271687da96e27ab6e22e741d61abb0a721045a
hugovom <hugo.om@gmail.com>
2015-07-10 13:33:20 -0500

    add fideicomisos es

---
+++ resources/views/fideicomisos_es.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.master')
+@section('body_class', 'reportajes')
+
+@section('title', 'Qué es un Fideicomiso')
+@section('description', "Los fideicomisos son")
+
+@section('content')
+<div class="main">
+	<nav class="breadcrumb">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-10 col-sm-offset-1">
+					<ul>
+						<li><a href="/">Inicio</a>
+						<li>¿Qué es un Fideicomiso?</li>
+					</ul>
+					<h1>¿Qué es un fideicomiso?</h1>
+				</div>
+			</div>
+		</div>
+	</nav>
+</div>
+
+<section class="list_news">
+</section>
+
+@endsection
