d303fd1ee69a4eb3c058b545e94e13df227f6421
hugovom <hugo.om@gmail.com>
2015-07-10 13:39:16 -0500

    add who

---
+++ resources/views/who.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.master')
+@section('body_class', 'reportajes')
+
+@section('title', '¿quiénes somos?')
+@section('description', "Equipo detrás de los fideicomisos")
+
+@section('content')
+<div class="main">
+	<nav class="breadcrumb">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-10 col-sm-offset-1">
+					<ul>
+						<li><a href="/">Inicio</a>
+						<li>Quiénes somos</li>
+					</ul>
+					<h1>Quiénes Somos</h1>
+				</div>
+			</div>
+		</div>
+	</nav>
+</div>
+
+<section class="list_news">
+</section>
+
+@endsection
