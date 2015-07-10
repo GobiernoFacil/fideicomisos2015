2d451fb6de5a6c10322eec55f14c5521dc700ec1
hugovom <hugo.om@gmail.com>
2015-07-10 16:27:08 -0500

    add post

---
+++ resources/views/post.blade.php
@@ -0,0 +1,119 @@
+@extends('layouts.master')
+@section('body_class', 'post')
+
+@section('title', 'Reportajes')
+@section('description', "Los Reportajes de los fideicomisos")
+
+@section('content')
+<div class="main">
+	<nav class="breadcrumb">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<ul>
+						<li><a href="/">Inicio</a>
+						<li><a href="/reportajes">Reportajes</a></li>
+						<li>Festejos del Bicentenario</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</nav>
+</div>
+<!-- ends breadcrumb-->
+<article>
+	<header>
+		<div class="likeaboss">
+			<figure>
+				<img src="/images/mexico-bicentenario.jpg">
+			</figure>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-10 col-sm-offset-1">
+					<a href="#" class="category">Bicentenario</a>
+				<h1>Festejos del Bicentenario</h1>
+			<h2>A cinco años de los festejos del Bicentenario y a varios más de haberse encontrado diversas 
+
+irregularidades en el manejo de los fondos millonarios, no existe ningún funcionario sancionado, 
+
+ninguna instancia que haya recuperado los recursos ni tampoco que haya resarcido el daño.</h2>
+			<p> <span>17/abril/2015</span> | Lilia Saul </p>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-8 col-sm-offset-2">
+				<p class="lead">Más de 5 mil millones de pesos trasladados a diversas dependencias del gobierno federal terminaron por ser ejercidos por empresas con poca o nula transparencia.</p>
+
+<p>Existe al menos una decena de irregularidades que observó la Auditoría Superior de la Federación (ASF) y hasta la fecha, no se han reparado los daños.</p>
+
+<p>El gobierno federal encabezado por el entonces presidente Felipe Calderón Hinojosa decidió “transparentar” el uso de los recursos del Fideicomiso Bicentenario y al desmenuzar cada una de las facturas otorgadas a EL UNIVERSAL, éste encontró no sólo irregularidades en la forma en cómo se gastaron los recursos. Se observaron sobrecostos, ejercicio del gasto por parte de empresas inexpertas e incluso fondos no reportados.</p>
+
+<p>La idea de crear un fideicomiso no fue de Calderón Hinojosa, sino del ex presidente Vicente Fox Quesada. Antes de que concluyera su mandato, determinó que a través del Banco del Ejército (Banjército) se destinaran los fondos para “planear” con tiempo los festejos patrios. </p>
+	
+<p> Sin embargo, quien ejecutó los recursos fue su sucesor y fue desde la Oficina de la Presidencia –a cargo de Patricia Flores—quien determinó el destino final de los mismos. </p>
+
+<p>Por ejemplo, se autorizaron cuatro millones de pesos para la elaboración de El Coloso de las fiestas del Bicentenario de la Independencia, pero la figura terminó arrumbada a la intemperie en una bodega del Comité Administrador del Programa Federal de Construcción de Escuelas (CAPFCE) de la Secretaría de Educación Pública, donde hoy incluso se encuentra desaparecida.</p>
+
+
+<h2>El Coloso, obra millonaria, desaparecida</h2>
+
+<p>El Coloso, creación de Juan Carlos Canfield, compartió el espacio de una enorme explanada con un estacionamiento, propiedad de la SEP.</p>
+
+<p>Hoy, se desconoce su paradero.  El costo de la figura fue de cuatro millones de pesos para un trabajo de nueve meses, en donde participaron expertos de la Universidad Autónoma de Morelos y colaboradores del propio Canfield.</p>
+
+
+
+<div class="columna_frase left">
+	<p class="lafrase">"Jamás nadie ha presentado un documento, una grabación, algo, alguna prueba que sustente cualquiera de los infundios que se me hacen."
+		Patricia Flores Elizondo</p>
+</div>
+
+
+<p>Aunque la Secretaría de Educación Pública había ofrecido reubicar El Coloso en algún espacio público, el sexenio del ex presidente Felipe Calderón concluyó sin cumplir esa promesa.
+El Coloso de 20 metros de altura fue pensado por los productores de los festejos patrios de 2010 simplemente para lucirlo en un espectáculo para la televisión la noche del 15 de septiembre de 2010.</p>
+
+
+<p>Nada más. En el destino de la polémica figura no pensaron.</p>
+<p>Por los exorbitantes gastos para las fiestas del Bicentenario y la falta de transparencia, ha habido funcionarios acusados de corrupción y aunque se ha señalado la presunta implicación de Patricia Flores Elizondo, quien fuera jefa de la Oficina de la Presidencia, ella negó las acusaciones.</p>
+
+<p>"Jamás nadie ha presentado un documento, una grabación, algo, alguna prueba que sustente cualquiera de los infundios que se me hacen", asegura Flores.</p>
+<p>La Estela de Luz, ubicada sobre Paseo de la Reforma, en la ciudad de México, tuvo un costo de mil 300 millones de pesos. También se colocaron 34 relojes del Bicentenario, uno por cada entidad federativa, y dos adicionales en el DF y en Guanajuato, a un costo de 11 millones 726 mil pesos, cuyo destino se ignora.</p>
+
+<p>El Parque Bicentenario fue remodelado con un costo de mil 15 millones de pesos pero tardó años en concluir sus trabajos, por lo que no quedó terminado en 2010.</p>
+<p>Este diario documentó cómo se gastaron cuatro mil millones de pesos en los festejos del Bicentenario, superiores a los dos mil 900 que se habían informado.</p>
+
+<h2>El beneficio a las empresas</h2>
+<p>La compañía Corporación Interamericana de Entretenimiento (CIE), a través de filiales, se convirtió en los dos sexenios panistas, específicamente con el ex presidente Felipe Calderón, en una de las empresas más favorecidas en la obtención de contratos, los cuales incluyen parte de los cuatro mil millones de pesos gastados en la fiestas del Bicentenario de la Independencia y el Centenario de la Revolución.</p>
+<div class="columna_frase right">
+	<p class="lafrase">El Parque Bicentenario fue remodelado con un costo de mil 15 millones de pesos pero tardó años en concluir sus trabajos, por lo que no quedó terminado en 2010. </p>
+	</div>
+
+<p>La empresa, fundada en 1990 de acuerdo con su propia página de internet, cuenta como socio mayoritario al empresario Luis Alejandro Soberón Kuri, quien es el presidente ejecutivo del grupo de socios.</p>
+<p>Mediante una revisión a los contratos publicados en el portal gubernamental Compranet, se observa que esta empresa dedicada a los espectáculos no sólo obtuvo la mayoría de los contratos del fideicomiso que se creó para estas dos conmemoraciones, sino que obtuvo decenas de contrataciones con las distintas dependencias de gobierno.</p>
+<p>Para la obtención de los contratos, CIE participó sola y mediante empresas con nombres y razones sociales distintas, pero con una relación comercial entre ellas. Las empresas son: Make Pro S.A de C.V.; Creatividad y Espectáculos S.A. de C.V.; Unimarket S.A de C.V.; Bconnect Service S.A de C.V y Publitop S.A de C.V.</p>
+<p>Según Compranet, Make Pro S.A. de C.V., tiene un registro de 55 contratos con el gobierno. La mayoría de estos convenios están enfocados en la difusión de los programas sociales y fueron hechos para ser presentados en varias plataformas, como cine, radio y televisión.</p>
+<p>En diciembre de 2010, por ejemplo, obtuvo un contrato con Pronósticos para la Asistencia de Pública, para darles “servicio de difusión y promoción en medios complementarios” y el monto cobrado fue de 22 millones de pesos.</p>
+<p>Para el Bicentenario, la empresa hizo “una campaña de difusión integral de las conmemoraciones” y ahí sus honorarios alcanzaron la suma de 84 millones de pesos.</p>
+<p>Otra empresa ligada a CIE es Creatividad y Espectáculos S.A. de C.V. En este caso, Compranet arroja que de 2009 a 2011 obtuvo siete contratos por casi 400 millones de pesos. La empresa cobró 300 millones de pesos por los “servicios de administración del desarrollo, ejecución, montaje, operación, mantenimiento y desmontaje del Pabellón de México en la Expo Universal Shanghai 2010” y para las conmemoraciones patrias del mismo año hizo la gira “Orgullosamente mexicanos”, por la que cobró 518 millones de pesos.</p>
+<p>A través de Turissste, el Fideicomiso del Bicentenario erogó otros 609 millones de pesos. Parte del desglose de estos gastos los dio a conocer Carlos Loret de Mola, quien aseguró que nuevamente CIE y Best Video S.A de C.V., obtuvieron, mediante adjudicación directa, varios contratos.</p>
+<p>Best Video S.A. de C.V., hizo el espectáculo multimedia en la fachada de Palacio Nacional y cobró casi 50 millones de pesos, pero se asoció con Unimarket S.A. de C.V., —otra subsidiaria de CIE— para hacer el montaje de este evento. La segunda empresa ganó casi 15 millones de pesos.</p>
+<p>Por si esto fuera poco, estas empresas volvieron unir esfuerzos y consiguieron otro contrato por 57 millones de pesos para organizar el “tradicional Grito de la Independencia” en el Zócalo capitalino, así como “la verbena popular en el interior de Palacio Nacional”, la noche del 15 de septiembre de 2010.</p>
+<p>Según los oficios, la presentación de la convocatoria para la construcción del monumento al Bicentenario — la Estela de Luz— costó 15 millones de pesos: la primera piedra, dos millones; encender el fuego conmemorativo, más de cinco millones. Dos desfiles de niños, 60 millones y el lanzamiento de la cartelera, 22 millones.</p>
+<h2>Otras empresas</h2>
+<p>El dinero para festejar la Independencia de México y la Revolución también alcanzó para contratar a empresas extranjeras, quienes hicieron los eventos de más impacto y con lo que se buscó llegar al clímax de las celebraciones.</p>
+<p>La compañía australiana Instantia organizó los festejos el 15 de septiembre a lo largo de Paseo de la Reforma y en parte del Centro Histórico, por lo que obtuvo como pago 670 millones de pesos.</p>
+<p>La empresa francesa Royal de Lux hizo durante varios días en Guadalajara, Jalisco, un espectáculo callejero con títeres gigantes, el cual tuvo presupuesto asignado de 89 millones.</p>
+<p>El dinero restante se distribuyó en otras empresas, como Tripe iii, que construyó, con irregularidades, La Estela de Luz, así como diversas personas físicas.
+</p>
+
+			</div>
+		</div>
+	</div>
+</article>
+
+@endsection
