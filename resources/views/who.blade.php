017edefef06cbfe0748ad10c4a3bc6819ccb1e2a
hugovom <hugo.om@gmail.com>
2015-10-06 18:09:40 -0500

    fix #36

--- resources/views/who.blade.php
+++ resources/views/who.blade.php
@@ -22,6 +22,24 @@
 </div>
 
 <section class="list_news">
+	<div class="container">
+		<div class="row">
+				<div class="col-sm-8 col-sm-offset-2">
+					<h3>Coordinación</h3>
+					<p>Lilia Saúl, Editora Periodismo de Datos EL UNIVERSAL de México</p>
+					<h3>Textos e investigación</h3>
+					<p>Lilia Saúl / Daniela Guazo</p>
+					<h3>Análisis de Datos</h3>
+					<p>Daniela Guazo / César Gutiérrez / <a href="http://gobiernofacil.com/">Gobierno Fácil</a></p>
+					<h3>Visualización de Datos</h3>
+					<p><a href="http://gobiernofacil.com/">Gobierno Fácil</a></p>
+					<h3>Desarrollo y Diseño Web</h3>
+					<p><a href="http://gobiernofacil.com/">Gobierno Fácil</a></p>
+					
+					<p>Escríbenos un correo: <a href="mailto:periodismodatos@eluniversal.com.mx">periodismodatos@eluniversal.com.mx</a></p>
+				</div>
+		</div>
+	</div>
 </section>
 
 @endsection
