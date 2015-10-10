920e75fc3dbc7c347ddc549272bb4a2e18a26249
hugovom <hugo.om@gmail.com>
2015-10-10 18:53:14 -0500

    update opendate page

--- resources/views/opendata.blade.php
+++ resources/views/opendata.blade.php
@@ -15,8 +15,6 @@
 						<li>Datos Abiertos</li>
 					</ul>
 					<h1>Datos Abiertos</h1>
-					<p><a href="/csv/trusts.csv">Fideicomisos 2006 - 2014 (csv)</p>
-					<p><a href="/csv/trusts_2015.csv">Fideicomisos 2015 primer y segundo trimestre (csv)</p>
 				</div>
 			</div>
 		</div>
@@ -24,6 +22,29 @@
 </div>
 
 <section class="list_news">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-7 col-sm-offset-1">
+				
+				<h3>Base de datos de Fideicomisos Públicos en México 2006 al 2014</h3>
+				<p><a href="/csv/trusts.csv" class="btn_link">Fideicomisos 2006 - 2014 (csv)</a></p>
+				<div class="divider"></div>
+				<h3>Base de datos de Fideicomisos Públicos en México 2015 (primer y segundo trimestre)</h3>
+				<p><a href="/csv/trusts_2015.csv" class="btn_link">Fideicomisos 2015 primer y segundo trimestre (csv)</a></p>
+				<div class="divider"></div>
+				<h3>Código Abierto</h3>
+				<p>Contribuye o replica este proyecto.</p>
+				<p><a href="https://github.com/GobiernoFacil/fideicomisos2015" class="btn_link">Código Abierto del proyecto</a></p>
+			</div>
+			<aside class="col-xs-10 col-xs-offset-1 col-sm-3">
+				<div class="row">
+					<div class="col-sm-10 col-sm-offset-1">
+						@include('layouts.sidebar')
+					</div>
+				</div>
+			</aside>
+		</div>
+	</div>
 </section>
 
 @endsection
