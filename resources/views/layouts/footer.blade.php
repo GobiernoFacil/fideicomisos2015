1c9ed9e8c7826a3b46cebb470d9be5ef7d26dec4
elcoruco <el.coruco@gmail.com>
2016-01-25 01:11:03 -0600

    fix the footer and some CSS

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -16,20 +16,65 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-4">
+			  <!--
 				<p>© 2015 EL UNIVERSAL Online S.A. de C.V</p>
+
 				<p><a href="{{url('quienes-somos')}}">Créditos</a></p>
+				-->
 			</div>
 			<div class="col-sm-4">
 				<span class="eluniversal_footer">El Universal</span>
 			</div>
 			<div class="col-sm-4 right">
+			<!--
 				<p>Forjado artesanalmente por</p>
 				<a href="http://gobiernofacil.com/" class="gobiernofacil">Gobierno Fácil</a>
 				<a href="http://www.morlan.mx" class="morlan" style="display:inline-block; width:199px;"><img src="/logo-morlan.svg" alt="morlan"></a>
 				<a href="http://fundar.org.mx/" class="fundar" style="display:inline-block; width:199px; margin-top:1.6em;"><img src="/logo-fundar.jpg" alt="Fundar"></a>
+				-->
 			</div>
 		</div>
 	</div>
+
+	<div class="container">
+		<div class="row" style="margin:3.5em 0"> 
+		  <div class="col-sm-4" style="text-align:center">
+		    <p>Forjado artesanalmente por</p>
+				<a href="http://gobiernofacil.com/" class="gobiernofacil">Gobierno Fácil</a>
+			</div>
+			<div class="col-sm-4" style="text-align:center">
+				<a href="http://www.morlan.mx" class="morlan" style="display:inline-block; width:199px;"><img src="/logo-morlan.svg" alt="morlan"></a>
+			</div>
+			<div class="col-sm-4 right" style="text-align:center">
+				<a href="http://fundar.org.mx/" class="fundar" style="display:inline-block; width:199px; margin-top:1.6em;"><img src="/logo-fundar.jpg" alt="Fundar"></a>
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="row" style="margin:3.5em 0">
+			<div class="col-sm-4">
+			  <!--
+				<p>© 2015 EL UNIVERSAL Online S.A. de C.V</p>
+
+				<p><a href="{{url('quienes-somos')}}">Créditos</a></p>
+				-->
+			</div>
+			<div class="col-sm-4" style="text-align:center">
+				<p>© 2015 EL UNIVERSAL Online S.A. de C.V</p>
+			</div>
+			<div class="col-sm-4 right">
+			<!--
+				<p>Forjado artesanalmente por</p>
+				<a href="http://gobiernofacil.com/" class="gobiernofacil">Gobierno Fácil</a>
+				<a href="http://www.morlan.mx" class="morlan" style="display:inline-block; width:199px;"><img src="/logo-morlan.svg" alt="morlan"></a>
+				<a href="http://fundar.org.mx/" class="fundar" style="display:inline-block; width:199px; margin-top:1.6em;"><img src="/logo-fundar.jpg" alt="Fundar"></a>
+				-->
+			</div>
+		</div>
+	</div>
+
+
 </footer>
 <!-- <script src="/js/bower_components/jquery/dist/jquery.min.js"></script> -->
 <script src="//api.html5media.info/1.1.8/html5media.min.js"></script>
@@ -45,3 +90,20 @@
   ga('send', 'pageview');
 
 </script>
+
+<script>window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+ 
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+ 
+  return t;
+}(document, "script", "twitter-wjs"));</script>
--- resources/views/who.blade.php
+++ resources/views/who.blade.php
@@ -25,18 +25,38 @@
 	<div class="container">
 		<div class="row">
 				<div class="col-sm-8 col-sm-offset-2">
+					
+					
+					<h2>Créditos</h2>
 					<h3>Coordinación</h3>
 					<p>Lilia Saúl, Editora Periodismo de Datos EL UNIVERSAL de México</p>
 					<h3>Textos e investigación</h3>
-					<p>Lilia Saúl / Daniela Guazo</p>
+					<p>Lilia Saúl / Daniela Guazo / <a href="http://fundar.org.mx/">Fundar</a></p>
 					<h3>Análisis de Datos</h3>
 					<p>Daniela Guazo / César Gutiérrez / <a href="http://gobiernofacil.com/">Gobierno Fácil</a></p>
 					<h3>Visualización de Datos</h3>
-					<p><a href="http://gobiernofacil.com/">Gobierno Fácil</a></p>
-					<p><a href="http://www.morlan.mx">Morlan</a>
+					<p><a href="http://gobiernofacil.com/">Gobierno Fácil</a> / <a href="http://www.morlan.mx">Morlan</a></p>
 					<h3>Desarrollo y Diseño Web</h3>
 					<p><a href="http://gobiernofacil.com/">Gobierno Fácil</a></p>
-					
+
+					<!--
+					<h2>Síguenos en twitter</h2>
+					<h3>El Universal Data</h3>
+					<p><a href="https://twitter.com/liliasaul">@liliasaul</a> /
+					<a href="https://twitter.com/dany145">@dany145</a></p>
+
+					<h3>Gobierno fácil</h3>
+					<p><a href="https://twitter.com/elcoruco">@elcoruco</a></p>
+					<p><a href="https://twitter.com/hugovom">@hugovom</a></p>
+
+					<h3>Morlan</h3>
+					<p><a href="https://twitter.com/Landa23">@Landa23</a></p>
+					<p><a href="https://twitter.com/moaimx">@moaimx</a></p>
+
+					<h3>Fundar</h3>
+					<p><a href="https://twitter.com/FundarMexico">@FundarMexico</a></p>
+					-->
+
 					<p>Escríbenos un correo: <a href="mailto:periodismodatos@eluniversal.com.mx">periodismodatos@eluniversal.com.mx</a></p>
 				</div>
 		</div>
